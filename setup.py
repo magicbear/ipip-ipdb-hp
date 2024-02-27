87faf1dd7994fd46071e0c29fecc9cb1dd0e04c4
MagicBear <mb@bilibili.com>
2024-02-27 21:43:38 +0800

    add readme

--- setup.py
+++ setup.py
@@ -8,12 +8,29 @@ ext = Extension(
     py_limited_api=True  # opt-in flag for the usage of Python's limited API <python:c-api/stable>.
 )
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup_args = dict(
     name='ipip-ipdb-hp',
-    version="0.1.0",
+    version="0.1.1",
+    author="MagicBear",
+    author_email="magicbearmo@gmail.com",
+    description="IPIP.net non-officially supported IP database ipdb format parsing library",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    requires_python=">=3.7",
+    keywords=["ipdb", "ipip"],
+    license="Apache-2.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
     packages=find_packages(where="src"),  # list
     package_dir={"": "src"},  # mapping
     ext_modules=[ext],  # list
+    url='https://github.com/magicbear/ipip-ipdb-hp',
     scripts=[]  # list
 )
 
