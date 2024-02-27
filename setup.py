ac8656a623bc6eec8867f6628cee0f6f6ef9492e
MagicBear <mb@bilibili.com>
2024-02-27 21:19:10 +0800

    feat: pypi project

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
-#!/usr/bin/env python3
-# encoding: utf-8
+from setuptools import setup, find_packages, Extension
 
-from distutils.core import setup, Extension
+ext = Extension(
+    name='ipdb.ipdb',  # 'mypackage.mymodule'
+    sources=['src/ipdb/py-ipdb.c', 'src/ipdb/ipdb.c'],  # list of source files (to compile)
+    include_dirs=['src/ipdb'],  # list of directories to search for C/C++ header files (in Unix form for portability)
+    extra_link_args=["-ljson-c"],
+    py_limited_api=True  # opt-in flag for the usage of Python's limited API <python:c-api/stable>.
+)
 
-ipdb_module = Extension('ipdb', sources = ['py-ipdb.c', 'ipdb.c'], extra_link_args=["-ljson-c"])
+setup_args = dict(
+    name='ipip-ipdb-hp',
+    version="0.1.0",
+    packages=find_packages(where="src"),  # list
+    package_dir={"": "src"},  # mapping
+    ext_modules=[ext],  # list
+    scripts=[]  # list
+)
 
-setup(name='ipdb',
-      version='0.1.0',
-      description='IPIP.net officially supported IP database ipdb format parsing library for python',
-      ext_modules=[ipdb_module])
+setup(**setup_args)
